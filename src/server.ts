5f313a1be19008396c9362332ba26953e0febe18
admin@gentoro.com <altamiro@bornio.com>
2024-12-12 10:06:26 -0500

    implement fallback handler to proxy all requests, also changed github repository url

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,6 @@ import {
 } from "./types.js";
 import axios from "axios";
 import {
-    CallToolRequest,
-    CallToolRequestSchema,
-    CallToolResult,
-    CallToolResultSchema,
     ErrorCode,
     InitializeRequest,
     InitializeRequestSchema,
@@ -15,11 +11,7 @@ import {
     JSONRPCRequest,
     JSONRPCRequestSchema,
     LATEST_PROTOCOL_VERSION,
-    ListToolsRequest,
-    ListToolsRequestSchema,
-    ListToolsResult,
-    ListToolsResultSchema,
-    McpError
+    McpError, Request, Result, ResultSchema
 } from "@modelcontextprotocol/sdk/types.js";
 import {RequestHandlerExtra} from "@modelcontextprotocol/sdk/shared/protocol.js";
 import {HttpTransport} from "./transport.js";
@@ -32,36 +24,60 @@ export class GentoroServer {
     private _server: Server | null = null;
     constructor() {
         this._config = {
-            apiKey: process.env.GENTORO_API_KEY || null,
-            bridgeUid: process.env.GENTORO_BRIDGE_UID || null,
-            baseUrl: process.env.GENTORO_BASE_URL || null
+            apiKey: process.env.GENTORO_API_KEY || '11ff1da12709c0f9e8c95c77de1f15707840b0a9b5e8967f9262ce3973c835e0',
+            bridgeUid: process.env.GENTORO_BRIDGE_UID || '553upxFZXx2GhCTTZAj55q',
+            baseUrl: process.env.GENTORO_BASE_URL || 'https://stage.gentoro.com/api'
         }
         this._transport = new HttpTransport(this._config);
     }
 
     async start():Promise<void> {
         const initializeResult: InitializeResult = await this.initialize();
-        console.log('Initializing with capabilities:', initializeResult.capabilities);
         this._server = new Server({
             name: "gentoro-mcp-nodejs-server",
             version: "0.0.1",
         }, {
             capabilities: initializeResult.capabilities,
         });
-
-        this._server.setRequestHandler( InitializeRequestSchema, this.InitializeRequestHandler.bind(this));
-        this._server.setRequestHandler( ListToolsRequestSchema, this.ListToolsRequestHandler.bind(this));
-        this._server.setRequestHandler( CallToolRequestSchema, this.CallToolRequestHandler.bind(this));
-
-        this._server.onerror = this.onErrorHandler.bind(this)
+        this._server.fallbackRequestHandler =  (request: Request) => this.FallbackRequestHandler(request);
         process.on('SIGINT', this.SignalHandler.bind(this));
 
         const transport = new StdioServerTransport();
         return this._server.connect(transport);
     }
 
-    onErrorHandler (error: Error ): void {
-        console.error("[MCP Error]", error);
+    logMessage(level: "error" | "debug" | "info" | "notice" | "warning" | "critical" | "alert" | "emergency", data: string): void {
+        if( this._server != null ) {
+            debugger;
+            this._server.sendLoggingMessage({
+                level: level,
+                data: data,
+            });
+        }
+    }
+
+    async FallbackRequestHandler (request: Request ): Promise<Result> {
+        const cmd : JSONRPCRequest = JSONRPCRequestSchema.parse({
+            jsonrpc: '2.0',
+            id: Date.now(),
+            ... request
+        })
+
+        this.logMessage("debug", "Sending command to Gentoro API: " + JSON.stringify(cmd));
+
+        return await this._transport.send(cmd)
+            .then(response => {
+                this.logMessage("debug", "Received response from Gentoro API: " + JSON.stringify(response));
+                return ResultSchema.parse(response.result) as Result
+            }).catch(error => {
+                if (axios.isAxiosError(error)) {
+                    throw new McpError(
+                        ErrorCode.InternalError,
+                        `Gentoro API error: ${error.response?.data.message ?? error.message}`
+                    );
+                }
+                throw error;
+            });
     }
 
     async SignalHandler (): Promise<never> {
@@ -69,7 +85,6 @@ export class GentoroServer {
             if( this._server != null ) {
                 this._server.close()
                     .then(() => {
-                        console.log('Processed ended successfully');
                         process.exit(0);
                     })
             }
@@ -89,53 +104,6 @@ export class GentoroServer {
             }
         });
         return this.InitializeRequestHandler(initializeRequest, null);
-    }
-
-    async ListToolsRequestHandler (): Promise<ListToolsResult> {
-        const request: ListToolsRequest = ListToolsRequestSchema.parse({
-            method: 'tools/list',
-            params: {}
-        }) as ListToolsRequest;
-
-        const cmd : JSONRPCRequest = JSONRPCRequestSchema.parse({
-            jsonrpc: '2.0',
-            id: Date.now(),
-            ... request
-        })
-
-        const result:ListToolsResult = await this._transport.send(cmd)
-            .then(response => ListToolsResultSchema.parse(response.result) as ListToolsResult).catch(error => {
-                if (axios.isAxiosError(error)) {
-                    throw new McpError(
-                        ErrorCode.InternalError,
-                        `Gentoro API error: ${error.response?.data.message ?? error.message}`
-                    );
-                }
-                throw error;
-            });
-
-        return result;
-    }
-
-    async CallToolRequestHandler ( data: CallToolRequest ): Promise<CallToolResult> {
-        const cmd : JSONRPCRequest = JSONRPCRequestSchema.parse({
-            jsonrpc: '2.0',
-            id: Date.now(),
-            ... data
-        })
-
-        const result:CallToolResult = await this._transport.send(cmd)
-            .then(response => CallToolResultSchema.parse(response.result) as CallToolResult).catch(error => {
-                if (axios.isAxiosError(error)) {
-                    throw new McpError(
-                        ErrorCode.InternalError,
-                        `Gentoro API error: ${error.response?.data.message ?? error.message}`
-                    );
-                }
-                throw error;
-            });
-        console.log('CallToolResult:', result);
-        return result;
     }
 
     async InitializeRequestHandler ( data: InitializeRequest , extra: RequestHandlerExtra | null ): Promise<InitializeResult> {
